e61c7be3fce27df380f2d08a1a8a0c4e186c3b7d
IceMoonMagic <icemoonmagic@gmail.com>
2025-04-26 14:43:39 -0500

    Fix vite base for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import vue from "@vitejs/plugin-vue"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
+  base: "derail-valley-consist-tracker",
+  build: { sourcemap: true },
 })
